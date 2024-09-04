94230e3c2b71924cb6efbf9247037b2382fa5ee9
wbezenah <wbezenah29@gmail.com>
2024-09-04 15:22:22 -0400

    updates to shapes helper functionality

--- src/app/shared/shapes-math.ts
+++ src/app/shared/shapes-math.ts
@@ -12,12 +12,20 @@ function calcTriAngles(a: number, b: number, c: number, round_to_n = 4) {
     return {theta_ab: ab, theta_bc: bc, theta_ac: ac};
 }
 
+export enum ShapeType {
+    SQUARE = 'square',
+    RECTANGLE = 'rectangle',
+    TRIANGLE = 'triangle',
+    CIRCLE = 'circle',
+    REG_POLY = 'regpoly'
+}
+
 interface Shape { 
-    readonly shape_type: string;
+    readonly shape_type: ShapeType;
 }
 
 class Square implements Shape {
-    shape_type = 'Square';
+    shape_type = ShapeType.SQUARE;
 
     constructor(public sideLength: number) { }
 
@@ -31,7 +39,7 @@ class Square implements Shape {
 }
 
 class Rectangle implements Shape {
-    shape_type = 'Rectangle';
+    shape_type = ShapeType.RECTANGLE;
 
     constructor(public length: number, public width: number) { }
 
@@ -45,7 +53,7 @@ class Rectangle implements Shape {
 }
 
 class Triangle implements Shape {
-    shape_type = 'Triangle';
+    shape_type = ShapeType.TRIANGLE;
 
     private _angles: {
         theta_ab: number;
@@ -80,14 +88,14 @@ class Triangle implements Shape {
 }
 
 class Circle implements Shape {
-    shape_type = 'Circle';
+    shape_type = ShapeType.CIRCLE;
 
     constructor(public radius: number) { }
 }
 
 class RegPolygon implements Shape {
     static readonly MIN_SIDES: number = 3;
-    shape_type = 'RegPolygon';
+    shape_type = ShapeType.REG_POLY;
 
     private _interior_angle: number;
     private _exterior_angle: number;
@@ -116,21 +124,21 @@ type side_spec = 'max' | 'min' | number;
 
 function toSquare(shape: Shape, use_for_side: side_spec = 'max'): Square {
     switch(shape.shape_type) {
-        case 'Square':
+        case ShapeType.SQUARE:
             return shape as Square;
-        case 'Rectangle':
+        case ShapeType.RECTANGLE:
             const rect = shape as Rectangle;
             return new Square(use_for_side === 'max' ? Math.max(rect.length, rect.width) : use_for_side === 'min' ? Math.min(rect.length, rect.width) : use_for_side >= 1 ? rect.width : rect.length);
 
-        case 'Triangle':
+        case ShapeType.TRIANGLE:
             const tri_sides = (shape as Triangle).sides;
             return new Square(use_for_side === 'max' ? Math.max(...Object.values(tri_sides)) : use_for_side === 'min' ? Math.min(...Object.values(tri_sides)) : use_for_side >= 2 ? tri_sides.side_c : use_for_side === 1 ? tri_sides.side_b : tri_sides.side_a);
 
-        case 'Circle':
+        case ShapeType.CIRCLE:
             const circle = shape as Circle;
             return new Square(2 * circle.radius);
 
-        case 'RegPolygon':
+        case ShapeType.REG_POLY:
             const poly = shape as RegPolygon;
             return new Square(poly.side_length);
 
@@ -141,24 +149,24 @@ function toSquare(shape: Shape, use_for_side: side_spec = 'max'): Square {
 
 function toRectangle(shape: Shape, use_for_length: side_spec = 'max', use_for_width: side_spec = 'max'): Rectangle {
     switch(shape.shape_type) {
-        case 'Square':
+        case ShapeType.SQUARE:
             const square = shape as Square;
             return new Rectangle(square.sideLength, square.sideLength);
             
-        case 'Rectangle':
+        case ShapeType.RECTANGLE:
             return shape as Rectangle;
 
-        case 'Triangle':
+        case ShapeType.TRIANGLE:
             const tri_sides = (shape as Triangle).sides;
             const nLength = use_for_length === 'max' ? Math.max(...Object.values(tri_sides)) : use_for_length === 'min' ? Math.min(...Object.values(tri_sides)) : use_for_length >= 2 ? tri_sides.side_c : use_for_length === 1 ? tri_sides.side_b : tri_sides.side_a;
             const nWidth = use_for_width === 'max' ? Math.max(...Object.values(tri_sides)) : use_for_width === 'min' ? Math.min(...Object.values(tri_sides)) : use_for_width >= 2 ? tri_sides.side_c : use_for_width === 1 ? tri_sides.side_b : tri_sides.side_a;
             return new Rectangle(nLength, nWidth);
 
-        case 'Circle':
+        case ShapeType.CIRCLE:
             const circle = shape as Circle;
             return new Rectangle(circle.radius * 2, circle.radius * 2);
 
-        case 'RegPolygon':
+        case ShapeType.REG_POLY:
             const poly = shape as RegPolygon;
             return new Rectangle(poly.side_length, poly.side_length);
 
@@ -169,22 +177,22 @@ function toRectangle(shape: Shape, use_for_length: side_spec = 'max', use_for_wi
 
 function toCircle(shape: Shape, use_for_radius: side_spec = 'max'): Circle {
     switch(shape.shape_type) {
-        case 'Square':
+        case ShapeType.SQUARE:
             const square = shape as Square;
             return new Circle(square.sideLength / 2);
             
-        case 'Rectangle':
+        case ShapeType.RECTANGLE:
             const rect = shape as Rectangle;
             return new Circle(use_for_radius === 'max' ? Math.max(rect.length, rect.width) / 2 : use_for_radius === 'min' ? Math.min(rect.length, rect.width) / 2 : use_for_radius >= 1 ? rect.width / 2 : rect.length / 2);
 
-        case 'Triangle':
+        case ShapeType.TRIANGLE:
             const tri_sides = (shape as Triangle).sides;
             return new Circle(use_for_radius === 'max' ? Math.max(...Object.values(tri_sides)) : use_for_radius === 'min' ? Math.min(...Object.values(tri_sides)) : use_for_radius >= 2 ? tri_sides.side_c : use_for_radius === 1 ? tri_sides.side_b : tri_sides.side_a);
 
-        case 'Circle':
+        case ShapeType.CIRCLE:
             return shape as Circle;
 
-        case 'RegPolygon':
+        case ShapeType.REG_POLY:
             const poly = shape as RegPolygon;
             return new Circle(poly.side_length);
 
@@ -196,23 +204,23 @@ function toCircle(shape: Shape, use_for_radius: side_spec = 'max'): Circle {
 function toRegPolygon(shape: Shape, use_for_side: side_spec = 'max', num_sides: number = RegPolygon.MIN_SIDES): RegPolygon {
     const rp_num_sides = Math.max(num_sides, RegPolygon.MIN_SIDES);
     switch(shape.shape_type) {
-        case 'Square':
+        case ShapeType.SQUARE:
             const square = shape as Square;
             return new RegPolygon(rp_num_sides, square.sideLength);
             
-        case 'Rectangle':
+        case ShapeType.RECTANGLE:
             const rect = shape as Rectangle;
             return new RegPolygon(rp_num_sides, use_for_side === 'max' ? Math.max(rect.length, rect.width) : use_for_side === 'min' ? Math.min(rect.length, rect.width) : use_for_side >= 1 ? rect.width : rect.length);
 
-        case 'Triangle':
+        case ShapeType.TRIANGLE:
             const tri_sides = (shape as Triangle).sides;
             return new RegPolygon(rp_num_sides, use_for_side === 'max' ? Math.max(...Object.values(tri_sides)) : use_for_side === 'min' ? Math.min(...Object.values(tri_sides)) : use_for_side >= 2 ? tri_sides.side_c : use_for_side === 1 ? tri_sides.side_b : tri_sides.side_a);
 
-        case 'Circle':
+        case ShapeType.CIRCLE:
             const circle = shape as Circle;
             return new RegPolygon(rp_num_sides, circle.radius);
 
-        case 'RegPolygon':
+        case ShapeType.REG_POLY:
             return shape as RegPolygon;
 
         default:
